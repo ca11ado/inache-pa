6020655e0d67c7d698ce4dbbdbfefdbb753abb98
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2017-01-15 13:15:14 +0300

    FEATURE-add-link-to-content-page - link added

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,15 +10,19 @@
  */
 
 import React from 'react';
+import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <Link to="/content">Content edit page</Link><br />
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+      </div>
     );
   }
 }
