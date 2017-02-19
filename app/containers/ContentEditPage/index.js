64423ec6cbdf959f258972733733ad2d27292aa0
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2017-02-19 23:51:57 +0300

    FEATURE-content-echo - not ended

--- app/containers/ContentEditPage/index.js
+++ app/containers/ContentEditPage/index.js
@@ -1,3 +1,6 @@
+import _ from 'lodash';
+import moment from 'moment';
+import styled from 'styled-components';
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
@@ -5,6 +8,31 @@ import { selectSections } from './selectors';
 import SubMenu from '../../components/SubMenu';
 import { SELECT_MENU } from './constants';
 import Loader from '../../components/Loader2';
+
+const FORMAT = 'DD.MM.YYYY';
+
+const ListItemWrapper = styled.li`
+  margin-bottom: 12px;
+`;
+
+const listItem = (item) => _.map(item, (property, key) => {
+  const result = property instanceof Array 
+    ? (<ul>ListItem(property, key)</ul>)
+    : (
+        <ListItemWrapper key={`ListItem-${key}`}>
+          <span><b>{key}:</b></span>
+          <span>{property}</span>
+        </ListItemWrapper>
+      );
+});
+
+const ListItem = (props) => {
+  const content = _.map(props.content, (letter) => (<li>{letter}</li>));
+
+  return (
+    <ul>{content}</ul>
+  );
+};
 
 export class ContentEditPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render () {
@@ -19,19 +47,8 @@ export class ContentEditPage extends React.PureComponent { // eslint-disable-lin
 
     const content = this.props.isLoaded
       ? (<Loader />)
-      : (
-        <ul>
-          {_.map(this.props.content, (item, index) => {
-            return (
-              <li key={`content-${index}`}>
-                <span>{item.date}</span>
-                <span> <b>{item.place}</b></span><br />
-                <span>{item.fullDescription}</span>
-              </li>
-            );
-          })}
-        </ul>
-      );
+    //: (<ListItem content={['t','e','s','t']} />);
+    : listItem(this.props.content);
 
     return (
       <div>
